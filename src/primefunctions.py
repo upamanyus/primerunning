f46882651688870aacbfef0fad5c3887125b2c8d
Upamanyu Sharma <upamanyu@umich.edu>
2017-01-27 22:44:16 -0500

    Added functionality to output values of the primewalking function.
    Fixed bug in walkingSum.

--- src/primefunctions.py
+++ src/primefunctions.py
@@ -1,11 +1,11 @@
 class PrimeFunctions:
 
-    def __init__(self,textName):
+    def __init__(self, textName):
         primeFile = open(textName)
         primeStrings = primeFile.read().split()
         primeFile.close()
         self.maxValue = int(primeStrings[0])
-        self.primes=[ int(num) for num in primeStrings[1:] ] 
+        self.primes = [int(num) for num in primeStrings[1:]]
 
     def runningStep(self, a, q, n):
         """returns 0 if largest prime less than or equal to n is congruent to a mod q, 1 otherwise"""
@@ -28,29 +28,29 @@ class PrimeFunctions:
         output = []
         index = 0
         sum = 0
-        for i in range(1,values[len(values)-1]+1):
-            sum += self.runningStep(a,q,i)
+        for i in range(1, values[len(values) - 1] + 1):
+            sum += self.runningStep(a, q, i)
             if i == values[index]:
                 output.append(sum)
                 index += 1
         return output
 
-    def walkingStep(self, a,q,p):
+    def walkingStep(self, a, q, p):
         """returns 1 if p is congruent to a mod q, 0 otherwise"""
-        if n > self.maxValue:
+        if p > self.maxValue:
             raise RuntimeError("Input outside of range")
         if p % q == a:
             return 1
         else:
             return 0
 
-    def walkingSum(self, a,q,x):
+    def walkingSum(self, a, q, n):
         """returns number of primes congruent to a mod q between for primes less than x"""
         if n > self.maxValue:
             raise RuntimeError("Input outside of range")
         sum = 0
         index = 0
-        while self.primes[index] <= x:
-            sum += self.walkingStep(a,q,self.primes[index])
+        while index < len(self.primes) and self.primes[index] <= n:
+            sum += self.walkingStep(a, q, self.primes[index])
             index += 1
         return sum
--- src/primerunning.py
+++ src/primerunning.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import primefunctions
 import argparse
 import genprimes
 
@@ -13,17 +14,33 @@ def main(argv):
     action_walk = subparser.add_parser('walk')
     action_run = subparser.add_parser('run')
 
-    action_gen_primes.add_argument('--out', type=argparse.FileType('w'),
-                                   default=sys.stdout)
-    action_gen_primes.add_argument('n', type=int, help='find primes up to this\
-            number')
-    action_walk.add_argument('file', type=argparse.FileType('r'))
+    action_gen_primes.add_argument('-o', '--outfile', type=argparse.FileType('w'),
+                                   default=sys.stdout,
+                                   help='the file to output to (default is stdout)')
+    action_gen_primes.add_argument('n', type=int, help=('find primes up to '
+                                   'this number'))
+    action_walk.add_argument('-i', '--infile', type=type(""),
+                             default=sys.stdin, help=('file containing prime '
+                             'numbers (default is to generate them internally)'))
+    action_walk.add_argument('a', type=int, help='consider the residue class of a')
+    action_walk.add_argument('q', type=int, help='consider residue class mod q')
+    action_walk.add_argument('n', type=int, help='the input of the walking function')
     args = parser.parse_args(argv[1:])
 
     if args.command == 'gen-primes':
-        with args.out as outfile:
+        with args.outfile as out:
             primes = genprimes.gen_primes(args.n)
-            outfile.write("\n".join([str(p) for p in primes]))
+            out.write(str(args.n) + "\n")
+            out.write("\n".join([str(p) for p in primes]) + "\n")
+    if args.command == 'walk':
+        try:
+            primeFunctions = primefunctions.PrimeFunctions(args.infile)
+            print(primeFunctions.walkingSum(args.a, args.q, args.n))
+        except RuntimeError as e:
+            print("Error: " + str(e))
+    else:
+
+        return 1
     return 0
 
 
