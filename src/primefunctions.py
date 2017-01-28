677c7f6107c3de5f18738557dfc555add252912f
Jaeyoon Kim <jaeykim@umich.edu>
2017-01-27 22:33:28 -0500

    PrimeFunctions now expects maximum value at the top of the file.
    runningSum maps increasing arrays to their outputs.

--- src/primefunctions.py
+++ src/primefunctions.py
@@ -4,10 +4,13 @@ class PrimeFunctions:
         primeFile = open(textName)
         primeStrings = primeFile.read().split()
         primeFile.close()
-        self.primes=[ int(num) for num in primeStrings ] 
+        self.maxValue = int(primeStrings[0])
+        self.primes=[ int(num) for num in primeStrings[1:] ] 
 
     def runningStep(self, a, q, n):
         """returns 0 if largest prime less than or equal to n is congruent to a mod q, 1 otherwise"""
+        if n > self.maxValue:
+            raise RuntimeError("Input outside of range")
         index = 0
         if n < 2:
             return 0
@@ -18,15 +21,24 @@ class PrimeFunctions:
         else:
             return 0
 
-    def runningSum(self, a, q, n):
+    def runningSum(self, a, q, values):
         """returns the sum of the runningStep from 1 to n"""
+        if values[len(values)-1] > self.maxValue:
+            raise RuntimeError("Input outside of range")
+        output = []
+        index = 0
         sum = 0
-        for i in range(1,n+1):
+        for i in range(1,values[len(values)-1]+1):
             sum += self.runningStep(a,q,i)
-        return sum
+            if i == values[index]:
+                output.append(sum)
+                index += 1
+        return output
 
     def walkingStep(self, a,q,p):
         """returns 1 if p is congruent to a mod q, 0 otherwise"""
+        if n > self.maxValue:
+            raise RuntimeError("Input outside of range")
         if p % q == a:
             return 1
         else:
@@ -34,17 +46,11 @@ class PrimeFunctions:
 
     def walkingSum(self, a,q,x):
         """returns number of primes congruent to a mod q between for primes less than x"""
+        if n > self.maxValue:
+            raise RuntimeError("Input outside of range")
         sum = 0
         index = 0
         while self.primes[index] <= x:
             sum += self.walkingStep(a,q,self.primes[index])
             index += 1
         return sum
-
-    def prevCongPrimes(self, a,q,x):
-        count = 0
-        index = 0
-        while self.primes[index] <= x:
-            if primes[index] % q == a:
-                count += 1
-        return count
