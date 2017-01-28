77788d85b7e9fff5060f178807be2e1b1b175cf1
Upamanyu Sharma <upamanyu@umich.edu>
2017-01-27 21:36:00 -0500

    Fixed the main command-line processor.
    The different interesting tasks (gen-primes, walking, running) are now
    different subcommands. This more clearly represents the desired
    behavior.

--- src/primerunning.py
+++ src/primerunning.py
@@ -2,26 +2,28 @@
 import argparse
 import genprimes
 
+
 def main(argv):
     parser = argparse.ArgumentParser(description='Prime running function')
+    subparser = parser.add_subparsers(dest='command')
 
-    action_group = parser.add_mutually_exclusive_group(required=True)
-    action_group.add_argument('-w', '--walking', action='store_true',
-                              help='generate output for the prime walking function')
-    action_group.add_argument('-r', '--running', action='store_true',
-                              help='generate output for the prime running function')
-    action_group.add_argument('-p', '--gen-primes', action='store', dest='num_primes', type=int,
-                              help='output a list of prime numbers up to NUM_PRIMES')
+    action_primes_help = 'generates a list of primes up to the given input'
+    action_gen_primes = subparser.add_parser('gen-primes',
+                                             help=action_primes_help)
+    action_walk = subparser.add_parser('walk')
+    action_run = subparser.add_parser('run')
+
+    action_gen_primes.add_argument('--out', type=argparse.FileType('w'),
+                                   default=sys.stdout)
+    action_gen_primes.add_argument('n', type=int, help='find primes up to this\
+            number')
+    action_walk.add_argument('file', type=argparse.FileType('r'))
     args = parser.parse_args(argv[1:])
 
-    if args.walking:
-        print("Not yet implemented!")
-    elif args.running:
-        print("Not yet implemented!")
-    elif args.num_primes:
-        print(genprimes.gen_primes(int(args.num_primes)))
-    else:
-        return 1
+    if args.command == 'gen-primes':
+        with args.out as outfile:
+            primes = genprimes.gen_primes(args.n)
+            outfile.write("\n".join([str(p) for p in primes]))
     return 0
 
 
