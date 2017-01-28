74ccfb018f67d703bab919017ebf9e4663cf8247
Jaeyoon Kim <jaeykim@umich.edu>
2017-01-27 21:38:23 -0500

    Added documentation for primefunction.py
    Fixed walkingStep for small values.

--- src/primefunctions.py
+++ src/primefunctions.py
@@ -7,28 +7,33 @@ class PrimeFunctions:
         self.primes=[ int(num) for num in primeStrings ] 
 
     def runningStep(self, a, q, n):
+        """returns 0 if largest prime less than or equal to n is congruent to a mod q, 1 otherwise"""
         index = 0
-        while self.primes[index] <= n:
+        if n < 2:
+            return 0
+        while self.primes[index+1] < n:
             index += 1
-        index -= 1
         if self.primes[index] % q == a:
             return 1
         else:
             return 0
 
     def runningSum(self, a, q, n):
+        """returns the sum of the runningStep from 1 to n"""
         sum = 0
         for i in range(1,n+1):
             sum += self.runningStep(a,q,i)
         return sum
 
     def walkingStep(self, a,q,p):
+        """returns 1 if p is congruent to a mod q, 0 otherwise"""
         if p % q == a:
             return 1
         else:
             return 0
 
     def walkingSum(self, a,q,x):
+        """returns number of primes congruent to a mod q between for primes less than x"""
         sum = 0
         index = 0
         while self.primes[index] <= x:
